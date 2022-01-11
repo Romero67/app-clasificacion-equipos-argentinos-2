7b97e96a98c2aefa4dadce6af0e108ca50c403c8
Romero67 <marianoromeroabadie@gmail.com>
2022-01-11 11:49:58 -0300

    make it better

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const configurations = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URI: process.env.DATABASE_MONGO,
-  TIME_GET_TEAMS: process.env.TIME_GET_TEAMS //10 segundos, expresado en milisegundos
+  MONGODB_URI: process.env.DATABASE_MONGO || 'mongodb:localhost/testApi',
+  TIME_GET_TEAMS: process.env.TIME_GET_TEAMS  || 10000//10 segundos, expresado en milisegundos
 };
 
 module.exports = configurations;
